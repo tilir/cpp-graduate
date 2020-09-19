62d1e8f769161438022886ccd90e401dc43017fd
Tilir <konstantin.vladimirov@gmail.com>
2020-09-19 16:28:05 +0300

    Completing files for 3rd logical seminar

---
+++ 03-concrete/rvo.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct foo {
+  foo() { cout << "foo::foo()" << endl; }
+  foo(const foo &) { cout << "foo::foo(const foo&)" << endl; }
+  foo(foo &&) { cout << "foo::foo(foo&&)" << endl; }
+  ~foo() { cout << "foo::~foo()" << endl; }
+};
+
+foo bar() {
+  foo local_foo;
+  return local_foo;
+}
+
+int main() { foo f = bar(); }
