351bc70992f29a6ca67377453db1f99276df5f05
Tilir <konstantin.vladimirov@gmail.com>
2021-09-11 16:38:35 +0300

    splitting cache header and driver

--- 01-basics/cache.cc
+++ 01-basics/cache.cc
@@ -6,46 +6,14 @@
 //
 //-----------------------------------------------------------------------------
 //
-//  Example for LRU cache in C++
+//  Example for LRU cache in C++: simple driver program
 //
 //----------------------------------------------------------------------------
 
 #include <cassert>
 #include <iostream>
-#include <iterator>
-#include <list>
-#include <unordered_map>
-#include <vector>
 
-template <typename T, typename KeyT = int> struct cache_t {
-  size_t sz_;
-  std::list<T> cache_;
-
-  using ListIt = typename std::list<T>::iterator;
-  std::unordered_map<KeyT, ListIt> hash_;
-
-  cache_t(size_t sz) : sz_(sz) {}
-
-  bool full() const { return (cache_.size() == sz_); }
-
-  template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
-    auto hit = hash_.find(key);
-    if (hit == hash_.end()) {
-      if (full()) {
-        hash_.erase(cache_.back());
-        cache_.pop_back();
-      }
-      cache_.push_front(slow_get_page(key));
-      hash_[key] = cache_.begin();
-      return false;
-    }
-
-    auto eltit = hit->second;
-    if (eltit != cache_.begin())
-      cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
-    return true;
-  }
-};
+#include "cache.hpp"
 
 // slow get page imitation
 int slow_get_page_int(int key) { return key; }
@@ -57,7 +25,7 @@ int main() {
 
   std::cin >> m >> n;
   assert(std::cin.good());
-  cache_t<int> c{m};
+  caches::cache_t<int> c{m};
 
   for (int i = 0; i < n; ++i) {
     int q;
---
+++ 01-basics/cache.hpp
@@ -0,0 +1,52 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT ILab
+// Slides: https://sourceforge.net/projects/cpp-lects-rus/files/cpp-graduate/
+// Licensed after GNU GPL v3
+//
+//-----------------------------------------------------------------------------
+//
+//  Example for LRU cache in C++
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <iterator>
+#include <list>
+#include <unordered_map>
+#include <vector>
+
+namespace caches {
+
+template <typename T, typename KeyT = int> struct cache_t {
+  size_t sz_;
+  std::list<T> cache_;
+
+  using ListIt = typename std::list<T>::iterator;
+  std::unordered_map<KeyT, ListIt> hash_;
+
+  cache_t(size_t sz) : sz_(sz) {}
+
+  bool full() const { return (cache_.size() == sz_); }
+
+  template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
+    auto hit = hash_.find(key);
+    if (hit == hash_.end()) {
+      if (full()) {
+        hash_.erase(cache_.back());
+        cache_.pop_back();
+      }
+      cache_.push_front(slow_get_page(key));
+      hash_[key] = cache_.begin();
+      return false;
+    }
+
+    auto eltit = hit->second;
+    if (eltit != cache_.begin())
+      cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
+    return true;
+  }
+};
+
+} // namespace caches
