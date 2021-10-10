22171b37d90dcfafe7d50502a9f989888cad661f
Tilir <konstantin.vladimirov@gmail.com>
2021-10-10 23:41:41 +0300

    Adding example with lvalue annotations

---
+++ 04-raii/lvannot.cc
@@ -0,0 +1,25 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT ILab
+// Slides: https://sourceforge.net/projects/cpp-lects-rus/files/cpp-graduate/
+// Licensed after GNU GPL v3
+//
+//-----------------------------------------------------------------------------
+//
+//  LValue annotations example: compilation error
+//
+//----------------------------------------------------------------------------
+
+#include <iostream>
+
+struct S {
+  int n = 0;
+  int& access() & { return n; }
+};
+
+int main() {
+  S x;
+  int& y = x.access(); // ok
+  int& z = S{}.access(); // compilation fail
+  std::cout << y << " " << z << std::endl;
+}
