9543b70ebedeac7e3830aaaffc2c30ecabb74835
Tilir <konstantin.vladimirov@gmail.com>
2022-04-10 00:28:51 +0300

    Adding example with copy flavours

---
+++ 17-algorithms/copy.cc
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT ILab
+// Slides: https://sourceforge.net/projects/cpp-lects-rus/files/cpp-graduate/
+// Licensed after GNU GPL v3
+//
+//------------------------------------------------------------------------------
+//
+// Different std::copy flavours
+//
+//------------------------------------------------------------------------------
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+  int arr[] = {2, 3, 5, 7, 11, 13, 17};
+  std::ostream_iterator<int> os(std::cout, " ");
+  std::vector<int> v(7);
+  std::copy_n(arr, 7, v.begin());
+
+  std::copy(v.begin(), v.end(), os);
+  std::cout << std::endl;
+
+  std::fill(v.begin(), v.end(), 0);
+  std::copy_if(arr, arr + 7, v.begin(), [](int i) { return (i % 3) == 1; });
+
+  std::copy(v.begin(), v.end(), os);
+  std::cout << std::endl;
+}
